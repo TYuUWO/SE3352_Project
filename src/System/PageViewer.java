83dd6ee3b18d0d992e8338e12aac99bf31deb777
TYuUWO <tyu263@uwo.ca>
2021-12-07 01:31:20 -0500

    a few skeletons created

--- src/System/PageViewer.java
+++ src/System/PageViewer.java
@@ -1,5 +1,35 @@
 package System;
 
-public class PageViewer {
+import java.util.ArrayList;
 
+public class PageViewer {
+	
+	private String userId;
+	private Page loginPage;
+	private ArrayList<Page> pageList = new ArrayList<>();
+	
+	public PageViewer(String userId, ArrayList<Page> pageList) {
+		//load the user credentials and get ready to access the database
+		this.userId = userId;
+		this.pageList = pageList;
+	}
+	
+	//user views page through link but must also be authorized to view the page
+	public Page getPage(String link, String userId){
+		Page page;
+		
+		//get page with link
+		for(int i=0;i<pageList.size();i++) {
+			if (pageList.get(i).getLink()==link) {
+				page = pageList.get(i);
+			}
+		}
+		
+		if (page.validate(userId, link)) {
+			return page;
+		}
+		else {
+			return loginPage;
+		}
+	}
 }
--- src/System/Resource.java
+++ src/System/Resource.java
@@ -1,5 +1,35 @@
 package System;
 
-public class Resource {
+import java.util.ArrayList;
 
+public class Resource {
+	//resources can either be pages or part of the content on a page
+
+	private ArrayList<String> userList = new ArrayList<>();
+	private String link;
+	private Content content;
+	
+	public Resource(ArrayList<String> users, String link, Content content) {
+		//initialize the users with permission to access and the page link
+		this.userList = users;
+		this.link = link;
+		//set content when resource is created; content cannot be changed
+		this.content = content;
+	}
+	
+	public String getLink() {
+		return link;
+	}
+	//no setLink because link cannot be changed
+	
+	//check whether user has permission to access the resource
+		public boolean validate(String userId) {
+			boolean valid = false;
+			for(int i=0;i<userList.size();i++) {
+				if (userList.get(i)==userId) {
+					valid = true;
+				}
+			}
+			return valid;
+		}
 }
