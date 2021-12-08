4207749584794ead676445cb65fd39f5f8a82f8e
James <jvu42@wuo.ca>
2021-12-08 05:45:45 -0500

    Merge page and resource class together

--- src/System/model/Page.java
+++ src/System/model/Page.java
@@ -1,22 +1,48 @@
 package System.model;
 
+import System.model.Comment;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class Page { // Here for layout of the page
-    List<Comment> commentList;
-    List<String> textList;
-    public Page(){
-
-    }
-
-    void displayPage() {
-
-    }
-    public void addComment(Comment comment){
-        commentList.add(comment);
-    }
-    public void addText(String text){
-        textList.add(text);
-    }
-
+public class Page {
+	
+	private ArrayList<String> userList = new ArrayList<>();
+	private String link;
+	private something content;
+	List<Comment> commentList;
+	List<String> textList;
+	
+	public Page(ArrayList<String> users, String link) {
+		//initialize the users with permission to access and the page link
+		this.userList = users;
+		this.link = link;
+	}
+	public void addComment(Comment comment){
+		commentList.add(comment);
+	}
+	public void addText(String text){
+		textList.add(text);
+	}
+	
+	public String getLink() {
+		return link;
+	}
+	//no setLink because link cannot be changed
+	
+	//method to change the page's content through the editor
+	public void setContent(putsomethinghere) {
+		this.content = putsomethinghere
+	}
+	
+	//check whether user has permission to access the page
+	public boolean validate(String userId) {
+		boolean valid = false;
+		for(int i=0;i<userList.size();i++) {
+			if (userList.get(i)==userId) {
+				valid = true;
+			}
+		}
+		return valid;
+	}
 }
--- src/System/model/Resource.java
+++ src/System/model/Resource.java
@@ -1,31 +1,50 @@
 package System.model;
 
+import System.model.EmbeddedFile;
+import System.model.Image;
+import System.model.Page;
+
+import java.util.ArrayList;
 import java.util.List;
-///////
-// List of Resources://////////
-//Image
-//EmbeddedFile
-//Text
-//CommentName
-//CommentDesc
-//CommentDate
-//Page
-//////
-public class Resource { // Image, files, text count as a resource, as well as the page themselves.
 
-    List<Page> pageList;
-    List<Image> imageList;
-    List<EmbeddedFile> embeddedFileList;
+public class Resource {
+	//resources can either be pages or part of the content on a page
 
-
-
-    void addToResource(Image image){ //upload an image to resource
-        imageList.add(image);
-    }
-    void addToResource(EmbeddedFile embeddedFile){ //upload an image to resource
-        embeddedFileList.add(embeddedFile);
-    }
-    void getAllFiles(){ // get images and files, send to file access
-
-    }
+	private ArrayList<String> userList = new ArrayList<>();
+	private String link;
+	private Content content;
+	List<Page> pageList;
+	List<Image> imageList;
+	List<EmbeddedFile> embeddedFileList;
+	
+	public Resource(ArrayList<String> users, String link, Content content) {
+		//initialize the users with permission to access and the page link
+		this.userList = users;
+		this.link = link;
+		//set content when resource is created; content cannot be changed
+		this.content = content;
+	}
+	void addToResource(Image image){ //upload an image to resource
+		imageList.add(image);
+	}
+	void addToResource(EmbeddedFile embeddedFile){ //upload an image to resource
+		embeddedFileList.add(embeddedFile);
+	}
+	void getAllFiles(){ // get images and files, send to file access
+	
+	public String getLink() {
+		return link;
+	}
+	//no setLink because link cannot be changed
+	
+	//check whether user has permission to access the resource
+		public boolean validate(String userId) {
+			boolean valid = false;
+			for(int i=0;i<userList.size();i++) {
+				if (userList.get(i)==userId) {
+					valid = true;
+				}
+			}
+			return valid;
+		}
 }
